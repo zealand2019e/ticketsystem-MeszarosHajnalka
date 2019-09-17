cca284ef7b81861229b7c91031d0545842ba24e8
mhajnalkavirag <mfloressa95@gmail.com>
2019-09-17 21:05:40 +0200

    Task 3

---
+++ TicketLibrary/MC.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TicketLibrary
+{
+   public class MC
+    {
+        public string licensePlate;
+        public DateTime date;
+        /// <summary>
+        /// Method, which returns the price of the vehicle
+        /// </summary>
+        /// <returns>125</returns>
+        public decimal Price()
+        {
+            return 125;
+        }
+        /// <summary>
+        /// Method, which returns the type of the vehicle
+        /// </summary>
+        /// <returns>MC</returns>
+        public string Vehicle()
+        {
+            return "MC";
+        }
+    }
+}
---
+++ TicketSystemTest/MCTestClass.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TicketLibrary;
+
+
+namespace TicketSystemTest
+{   [TestClass]
+   public class MCTestClass
+    {
+        MC mc = new MC();
+        /// <summary>
+        /// Testing the Price method, if it returns the value that we expected.
+        /// </summary>
+        [TestMethod]
+        public void PriceTestMethod()
+        {
+            decimal price = mc.Price();
+            Assert.AreEqual(125, price);
+        }
+        /// <summary>
+        /// Testing the Vehicle method, if it returns the value that we expected.
+        /// </summary>
+        [TestMethod]
+        public void VehicleTypeTestMethod()
+        {
+            string vehicle = mc.Vehicle();
+            Assert.AreEqual("MC",vehicle);
+
+        }
+    }
+}

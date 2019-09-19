01a18f0cffa29260c0db829c75027ae4b79912a2
mhajnalkavirag <mfloressa95@gmail.com>
2019-09-19 23:24:43 +0200

    Task 10

---
+++ OresundbronLibrary/Oresundbron.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using TicketLibrary;
+
+namespace OresundbronLibrary
+{
+    public class Oresundbron
+    {
+
+        public static double OresundPrice(object obj)
+        {
+            double x;
+
+            if (obj.GetType() == typeof(Car) || obj.GetType() == typeof(MC))
+            {
+                if (obj.GetType() == typeof(Car))
+                {
+                  return x = CarPriceChange(obj as Car);
+                }
+                if (obj.GetType() == typeof(MC))
+                {
+                    return x = MCPriceChange(obj as MC);
+                    
+                }
+            }
+
+            throw new Exception("Object type has to be 'Car' or 'MC'.");
+          
+        }
+
+        private static double MCPriceChange(MC mc)
+        {
+            double x;
+            if (mc.Brobizz == true)
+            {
+              return x = 73;
+            }
+
+            return x = 210;
+            
+        }
+
+        private static double CarPriceChange(Car car)
+        {
+            double x;
+            if (car.Brobizz == true)
+            {
+                return x = 161;
+            }
+
+            x = 410;
+            return x;
+        }
+
+
+        public static string VehicleType(object obj)
+        {
+            if (obj.GetType() == typeof(Car) || obj.GetType() == typeof(MC))
+            {
+                string s;
+
+                if (obj.GetType() == typeof(Car))
+                {
+                    return s = "Oresund car";
+                }
+                if (obj.GetType() == typeof(MC))
+                {
+                    return s = "Oresund MC";
+                }
+            }
+
+            throw new Exception("Object type has to be 'Car' or 'MC'.");
+        }
+    }
+}
---
+++ TicketSystemTest/OresundbronTest.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OresundbronLibrary;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using TicketLibrary;
+
+namespace TicketSystemTest
+{
+    [TestClass]
+    public class OresundbronTest
+    {
+
+
+
+        [TestMethod]
+        public void OresundPriceTest()
+        {
+            Car car = new Car("ABC-234", new DateTime(2019, 11, 11), false);
+            MC mc = new MC("ABC-123", new DateTime(2019, 11, 11), false);
+
+            Car car1 = new Car("ABC-234", new DateTime(2019, 11, 11), true);
+            MC mc1 = new MC("ABC-123", new DateTime(2019, 11, 11), true);
+
+            double carOresundPriceNoBrobizz = Oresundbron.OresundPrice(car);
+            double MCOresundPriceNoBrobizz = Oresundbron.OresundPrice(mc);
+
+            double carOresundPriceBrobizz = Oresundbron.OresundPrice(car1);
+            double MCOresundPriceBrobizz = Oresundbron.OresundPrice(mc1);
+
+            Assert.AreEqual(410 , carOresundPriceNoBrobizz);
+            Assert.AreEqual(210, MCOresundPriceNoBrobizz);
+
+            Assert.AreEqual(161, carOresundPriceBrobizz);
+            Assert.AreEqual(73, MCOresundPriceBrobizz);
+
+
+        }
+        [TestMethod]
+        [ExpectedException(typeof(Exception))]
+        public void OresundPriceMethodExecptionTest()
+        {
+
+            Vehicle veh = new Vehicle("plate",DateTime.Now,false);
+            Oresundbron.OresundPrice(veh);
+        }
+
+        [TestMethod]
+        public void OresundVehicleType()
+        {
+            Car car = new Car("ABC-234", new DateTime(2019, 11, 11), false);
+            MC mc = new MC("ABC-123", new DateTime(2019, 11, 11), false);
+
+            string carType = Oresundbron.VehicleType(car);
+            string mcType = Oresundbron.VehicleType(mc);
+
+            Assert.AreEqual("Oresund car", carType);
+            Assert.AreEqual("Oresund MC", mcType);
+
+        }
+    }
+}
